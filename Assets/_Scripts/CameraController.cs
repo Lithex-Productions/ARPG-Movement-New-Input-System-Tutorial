c807759c61e5d7f29bdd1dc5a873371ff3d90cbd
Brian Thex <brian.thex@gmail.com>
2021-02-04 13:30:25 -0600

    project upload

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace LP.ARPGNewInputTutorial
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform target = null;
+        [SerializeField] private Vector3 offset = new Vector3();
+        [SerializeField] private float pitch = 2f;
+
+        private void LateUpdate()
+        {
+            transform.position = target.position - offset;
+            transform.LookAt(target.position + Vector3.up * pitch);
+        }
+    }
+}
+
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
+using System;
+
+namespace LP.ARPGNewInputTutorial
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private InputAction movement = new InputAction();
+
+        [SerializeField] private LayerMask layerMask = new LayerMask();
+
+        private NavMeshAgent agent = null;
+        private Camera cam = null;
+
+        private void Start()
+        {
+            cam = Camera.main;
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void OnEnable()
+        {
+            movement.Enable();
+        }
+
+        private void OnDisable()
+        {
+            movement.Disable();
+        }
+
+        private void Update()
+        {
+            HandleInput();
+        }
+
+        private void HandleInput()
+        {
+            if (movement.ReadValue<float>() == 1)
+            {
+                Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100, layerMask))
+                {
+                    PlayerMove(hit.point);
+                }
+            }
+        }
+
+        private void PlayerMove(Vector3 location)
+        {
+            agent.SetDestination(location);
+        }
+    }
+}
+
